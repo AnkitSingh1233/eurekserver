f001bccedc1fc95a0e9bf5089a1d490ce1af8c80
hp <ankit.singh@krenai.com>
2023-07-18 14:23:23 +0530

    eureka Server

---
+++ BootMS_proj03-RAndDServer-EurekaServer/src/main/java/com/nt/BootMsProj01RAndDServerEurekaApplication.java
@@ -0,0 +1,14 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class BootMsProj01RAndDServerEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMsProj01RAndDServerEurekaApplication.class, args);
+	}
+
+}
